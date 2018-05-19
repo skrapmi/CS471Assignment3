5fbe354e1c0b4be2204530c019249fbfee11fa94
Timothy Parks <parkstimothyj@csu.fullerton.edu>
2018-05-18 18:21:53 -0700

    Create sendfilecli.py

---
+++ sendfilecli.py
@@ -0,0 +1,123 @@
+# *******************************************************************
+# This file illustrates how to send a file using an
+# application-level protocol where the first 10 bytes
+# of the message from client to server contain the file
+# size and the rest contain the file data.
+# *******************************************************************
+import socket
+import os
+import sys
+
+# Command line checks 
+#if len(sys.argv) < 2 or type(sys.argv[1]) != int:
+#    print "USAGE python " + "<server_name>" + " <port_number>" 
+#    sys.exit()
+
+# Server address
+serverAddr = sys.argv[1]
+
+# Server port
+serverPort = sys.argv[2]
+
+# Create a TCP socket
+connSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the server
+#connSock.connect((serverAddr, serverPort)) 
+
+def getFile(fileName):
+    # Connect to the server
+    #connSock.connect((serverAddr, serverPort))
+    
+    # Close the socket and the file
+    connSock.close()
+
+def putFile(fileName):
+    # Connect to the server
+    connSock.connect(('localhost', int(serverPort)))
+
+    # The number of bytes sent
+    numSent = 0
+
+    # The file data
+    fileData = None
+
+    # Open the file
+    fileObj = open(fileName, "r")
+
+    # Keep sending until all is sent
+    # Read 65536 bytes of data
+    fileData = fileObj.read(65536)
+
+    # Make sure we did not hit EOF
+    if fileData:
+        # Get the size of the data read
+        # and convert it to string
+        dataSizeStr = str(len(fileData))
+
+        # Prepend 0's to the size string
+        # until the size is 10 bytes
+        while len(dataSizeStr) < 10:
+            dataSizeStr = "0" + dataSizeStr
+
+        # Prepend 0's to the name string
+        # until the size is 10 bytes
+        while len(fileName) < 10:
+            fileName = "0" + fileName
+
+        # Prepend the size of the data to the
+        # file data.
+        fileData = "put" + fileName  + dataSizeStr + fileData
+
+        # The number of bytes sent
+        numSent = 0
+
+        # Send the data!
+        while len(fileData) > numSent:
+            numSent += connSock.send(fileData[numSent:])
+
+    print "Sent ", numSent, " bytes."
+
+    # Close the socket and the file
+    connSock.close()
+    fileObj.close()
+
+
+def ls():
+    # Connect to the server
+    connSock.connect((serverAddr, int(serverPort)))
+    connSock.send(" ls")
+
+    # Close the socket
+    connSock.close()
+
+def lls():
+    for line in commands.getstatusoutput('ls -l'):
+        print line    
+
+def exiting():
+    pass
+
+cont = True
+while cont:
+    response =  raw_input("ftp> ")
+    input_list = response.split()
+
+    if (input_list[0] == 'get'):
+        getFile(input_list[1])
+
+    elif (input_list[0] == 'put'):
+        putFile(input_list[1])
+
+    elif (input_list[0] == 'ls'):
+        ls()
+
+    elif (input_list[0] == 'lls'):
+        lls()
+
+    elif (input_list[0] == 'quit'):
+        exiting()
+        cont = False
+
+    else:
+        print "Invalid command"
