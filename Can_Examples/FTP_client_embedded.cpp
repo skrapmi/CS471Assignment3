0780bf8dd100e08ae2c78bd10f29e478483dc76b
Can <Jayteare@users.noreply.github.com>
2018-05-24 14:21:04 -0700

    Update FTP_client_embedded.cpp

--- Can_Examples/FTP_client_embedded.cpp
+++ Can_Examples/FTP_client_embedded.cpp
@@ -11,8 +11,7 @@ using namespace std;
 
 
 int main (int argc, char ** argv)
-{
-
+{	
 	try
 	{
 		cout << "Python Module Starting..." << endl;
@@ -20,31 +19,56 @@ int main (int argc, char ** argv)
 		
 		Py_Initialize();
 		boost::python::object ftp_ = boost::python::import("ftplib");
-		
-		cout << "Imported ftplib.." << endl;
-		
 		object main_module ((handle<>(borrowed(PyImport_AddModule("__main__")))));
 		object main_namespace = main_module.attr("__dict__");
+		object ignored = exec("", main_namespace);
 		
 		cout << "Started the Process.." << endl;
 		
-		object ignored = exec("", main_namespace);
+		ignored = exec("server_ = raw_input(\"Please type the server to connect: \")" , main_namespace);
+		ignored = exec("port_ = raw_input(\"Please type the port to connect: \")", main_namespace);
 		
 		ignored = exec("from ftplib import FTP", main_namespace);
-		
 		ignored = exec("ftp = FTP()", main_namespace);
-		ignored = exec("ftp.connect('localhost',2121)", main_namespace);
+		ignored = exec("ftp.connect(server_,port_)", main_namespace);
 		ignored = exec("ftp.login('user','12345')",main_namespace);
-		ignored = exec("ftp.retrlines('LIST')", main_namespace);
-		ignored = exec("filename = 'file_test_cpt.txt'",main_namespace);
-		ignored = exec("ftp.storbinary('STOR ' + filename, open(filename, 'rb'))", main_namespace);
-		ignored = exec("ftp.retrlines('LIST')", main_namespace);
-		ignored = exec("ftp.quit()", main_namespace);
 		
-		
-		//int five_squared = extract<int>(main_namespace["result"]);
-		//cout << five_squared << endl;
-		
+		string response_ = "";
+		while(true)
+		{
+			
+			cout << "ftp> ";
+			cin >> response_;
+			if(response_ == "get")
+			{
+				cin.ignore();
+				ignored = exec("filename_g = raw_input(\"Please type the file name: \")", main_namespace);
+				ignored = exec("ftp.retrbinary('RETR %s' % filename_g, file.write)", main_namespace);
+			}
+			else if(response_ == "put")
+			{
+				cin.ignore();
+				ignored = exec("filename = raw_input(\"Please type the file name: \")", main_namespace);
+				ignored = exec("ftp.storbinary('STOR ' + filename, open(filename, 'rb'))", main_namespace);
+			}
+			else if(response_ == "ls")
+			{
+				ignored = exec("ftp.retrlines('LIST')", main_namespace);
+			}
+			else if(response_ == "lls")
+			{
+				ignored = exec("ftp.retrlines('LIST')", main_namespace);
+			}
+			else if(response_ == "quit")
+			{	
+				ignored = exec("ftp.quit()", main_namespace);
+				break;
+			}
+			else
+			{
+				cout << "Invalid Command" << endl;
+			}
+		}
 	}
 	catch(error_already_set)
 	{
